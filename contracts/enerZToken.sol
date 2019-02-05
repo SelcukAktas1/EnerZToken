756cbb235a5274a52a731ffcafc9c3951e86a592
creator28728 <selcuk_aktas1@hotmail.com>
2019-02-05 18:01:06 +0300

    Contracts deployed on ropsten test network

--- contracts/enerZToken.sol
+++ contracts/enerZToken.sol
@@ -5,7 +5,11 @@ import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detaile
 
 
 contract EnerZToken is ERC20, ERC20Detailed {
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) 
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint8 _decimals
+        ) 
     ERC20Detailed(_name, _symbol, _decimals) public 
     {
 
